f89e7649839bdf68726265a0418db36ad07d392d
D Varun <21ag1a6617@gmail.com>
2025-12-16 06:49:35 +0530

    commit

---
+++ Fe/assets/app.js
@@ -0,0 +1,39 @@
+function login() {
+  alert("Login clicked (connect backend later)");
+}
+
+function registerUser() {
+  alert("Register clicked (connect backend later)");
+}
+function openPolicy(id) {
+  window.location.href = "policy-details.html?id=" + id;
+}
+function submitClaim() {
+  alert("Claim submitted successfully (backend later)");
+}
+
+function payNow() {
+  alert("Redirecting to payment gateway (mock)");
+}
+function createTicket() {
+  alert("Support ticket created (backend later)");
+}
+
+function saveProfile() {
+  alert("Profile updated successfully");
+}
+function approveAction(type) {
+  alert(type + " approved");
+}
+
+function rejectAction(type) {
+  alert(type + " rejected");
+}
+
+function exportCSV() {
+  alert("Exporting CSV (mock)");
+}
+
+function saveSettings() {
+  alert("Settings saved");
+}
